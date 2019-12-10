42b529e057d56c4313de95c690118e0d4956191a
rezkyal2@gmail.com <rezkyal2@gmail.com>
2019-12-11 00:50:30 +0700

    url setting

--- src/setting.js
+++ src/setting.js
@@ -1,4 +1,4 @@
-export const apiurl = "http://localhost:8080/"
-export const wsurl = "ws://localhost:8080/"
-// export const apiurl = "https://quiknote-backend.herokuapp.com/"
-// export const wsurl = "wss://quiknote-backend.herokuapp.com/"
+// export const apiurl = "http://localhost:8080/"
+// export const wsurl = "ws://localhost:8080/"
+export const apiurl = "https://quiknote-backend.herokuapp.com/"
+export const wsurl = "wss://quiknote-backend.herokuapp.com/"
